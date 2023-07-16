c92267c2513923d8df58cf094af43aa187bfb0f2
DS <dsrathorebsw1234@gmail.com>
2023-07-16 19:10:19 +0530

    added button

--- Components/Button.tsx
+++ Components/Button.tsx
@@ -6,12 +6,32 @@ interface ButtonProps {
     fullWidth?: boolean;
     large?: boolean;
     onClick: () => void;
+    disabled?: boolean;
+    outline?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, secondary, onClick, fullWidth, large }) => {
+const Button: React.FC<ButtonProps> = ({ label, secondary, onClick, fullWidth, large, disabled, outline }) => {
     return (
-        <div>Button</div>
-    )
+        <button disabled={disabled}
+            onClick={onClick}
+            className={`
+        disabled:opacity-70
+        disabled:cursor-not-allowed 
+        rounded-full 
+        font-semibold 
+        hover:opacity-80 
+        transition 
+        border-2 
+        ${fullWidth ? "w-full " : "w-fit"} 
+        ${secondary ? "bg-white" : "bg-sky-500"} 
+        ${secondary ? "border-black" : "border-sky-500"} 
+        ${large ? "text-xl" : "text-md"} 
+        ${large ? "px-5" : "px-4"} 
+        ${large ? "py-3" : "py-3"} 
+        ${outline ? "bg-transparent" : ""} 
+        ${outline ? "border-white" : ""} 
+        ${outline ? "text-white" : ""}`}>{label}</button>
+    );
 }
 
 export default Button
